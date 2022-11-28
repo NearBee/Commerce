ac75755359a2815471f3d272d76b591b82ef1fc8
Nathaniel Berube <Rexar14@gmail.com>
2022-11-28 14:27:07 -0600

    Model comments for testing purposes

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+# Remember that each time you change anything in auctions/models.py, you’ll need to first run python
+# manage.py makemigrations and then python manage.py migrate to migrate those changes to your database.
+
 
 class User(AbstractUser):
     pass
+    # username = models.CharField(max_length=18)
+    # password = models.CharField(max_length=18)
