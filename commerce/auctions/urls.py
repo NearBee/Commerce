ac8f3cd27e85cd4257b1412df5f022e447d64cc0
Nathaniel Berube <Rexar14@gmail.com>
2022-11-29 17:52:44 -0600

    added a WIP Listing page

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("createlisting", views.create_listing, name="creation"),
+    path("<str:listing>", views.active_listing, name="listing"),
 ]
