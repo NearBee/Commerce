792e8198b7c4601bcae4f16434e3c093c14c6af9
Nathaniel Berube <Rexar14@gmail.com>
2023-01-03 00:02:41 -0600

    Made it so admins could see "created_at" time

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from .models import User, Auction_Listing, Bid, Comment
 
 # Register your models here.
+class CommentsAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_at",)
+
 
 admin.site.register(User)
 admin.site.register(Auction_Listing)
 admin.site.register(Bid)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentsAdmin)
