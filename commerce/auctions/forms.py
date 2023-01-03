4cce7ed896e800f0668270478feeb986fe3ac612
Nathaniel Berube <Rexar14@gmail.com>
2023-01-03 00:03:04 -0600

    Added custom CSS to Comment and Bid fields

--- commerce/auctions/forms.py
+++ commerce/auctions/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms import Textarea
+from django.forms import Textarea, NumberInput
 from .models import Auction_Listing, Bid, Comment
 
 
@@ -15,6 +15,11 @@ class bid_forms(forms.ModelForm):
         model = Bid
         fields = "__all__"
         exclude = ["user", "auction_id"]
+        widgets = {
+            "new_bid": NumberInput(
+                attrs={"style": "height: 35px; width: 150px;", "class": "form-control"}
+            ),
+        }
 
 
 class comment_forms(forms.ModelForm):
@@ -24,6 +29,9 @@ class comment_forms(forms.ModelForm):
         exclude = ["user", "auction_id"]
         widgets = {
             "comments": Textarea(
-                attrs={"style": "height: 100px; width: 500px; resize: initial;"}
+                attrs={
+                    "style": "height: 100px; width: 100%; resize: initial;",
+                    "class": "form-control",
+                }
             ),
         }
