79c58f986ebd7076c1a0f11c21878bda20f380e2
Nathaniel Berube <Rexar14@gmail.com>
2023-01-06 02:44:22 -0600

    updated django message tags

--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: "secondary",
+    messages.INFO: "info",
+    messages.SUCCESS: "success",
+    messages.WARNING: "warning",
+    messages.ERROR: "danger",
+}
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
