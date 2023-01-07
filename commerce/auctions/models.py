18cca4079df800c7c9ebaf7781d30a4d96e85129
Nathaniel Berube <Rexar14@gmail.com>
2023-01-06 20:28:27 -0600

    removed help_text from from item_title

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -34,7 +34,8 @@ class Auction_Listing(models.Model):
     )
 
     item_title = models.CharField(
-        max_length=50, blank=False, help_text="50 Character limit"
+        max_length=50,
+        blank=False,
     )
 
     item_description = models.TextField(max_length=500, blank=False)
