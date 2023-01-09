c24f2df634b02a012a1b57df843216052931ec6d
Nathaniel Berube <Rexar14@gmail.com>
2023-01-09 12:39:46 -0600

    ngrok stuff changed

--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -37,7 +37,9 @@ SECRET_KEY = "6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []  # Put ngrok.io
+
+CSRF_TRUSTED_ORIGINS = []  # Put https:// ngrok.io
 
 
 # Application definition
